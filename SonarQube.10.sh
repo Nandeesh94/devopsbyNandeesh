4b1b31dfb2e308b823caa5eb2ce808781da068c5
Nandeesh94 <nandeesh.flipkart@gmail.com>
2025-01-17 19:08:48 +0530

    Update SonarQube.10.sh

--- SonarQube.10.sh
+++ SonarQube.10.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
-
 # Update and install required packages
+
 sudo yum update -y
 sudo amazon-linux-extras enable corretto17
 sudo yum install java-17-amazon-corretto -y
 sudo yum install wget unzip -y
 
-# Define SonarQube version and installation directory
-SONARQUBE_VERSION="10.2.0.77652"
-SONARQUBE_DIR="/opt/sonarqube-${SONARQUBE_VERSION}"
-DOWNLOAD_URL="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${SONARQUBE_VERSION}.zip"
-
-# Create a sonar user
-sudo useradd sonar
-
 # Download and install SonarQube
 cd /opt/
-sudo wget ${DOWNLOAD_URL}
-sudo unzip sonarqube-${SONARQUBE_VERSION}.zip
-sudo chown -R sonar:sonar sonarqube-${SONARQUBE_VERSION}
-sudo chmod -R 775 sonarqube-${SONARQUBE_VERSION}
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.2.0.77652.zip
+sudo unzip sonarqube-10.2.0.77652.zip
+sudo useradd sonar
+sudo chown sonar:sonar sonarqube-10.2.0.77652 -R
+sudo chmod -R 777 sonarqube-10.2.0.77652
 
 # Switch to the sonar user
-sudo su - sonar <<EOF
-cd ${SONARQUBE_DIR}/bin/linux-x86-64/
+sudo su - sonar 
+cd /opt
+cd sonarqube-10.2.0.77652/bin/linux-x86-64/
 ./sonar.sh start
-EOF
 
 # Print the status of SonarQube
-echo "Checking SonarQube status..."
-sudo su - sonar -c "${SONARQUBE_DIR}/bin/linux-x86-64/sonar.sh status"
+sudo su - sonar -c sonarqube-10.2.0.77652/bin/linux-x86-64/sonar.sh start
+sudo su - sonar -c sonarqube-10.2.0.77652/bin/linux-x86-64/sonar.sh status
 
 # Instructions to access SonarQube
-echo "--------------------------------------------------"
-echo "SonarQube 10.x installation complete!"
-echo "Access it via: http://<your-server-ip>:9000"
-echo "Default credentials: admin / admin"
-echo "Ensure port 9000 is open in your firewall/security group."
+#echo "--------------------------------------------------"
+#echo "SonarQube 10.x installation complete!"
+#echo "Access it via: http://<your-server-ip>:9000"
+#echo "Default credentials: admin / admin"
+#echo "Ensure port 9000 is open in your firewall/security group."
