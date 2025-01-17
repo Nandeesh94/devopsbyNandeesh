9260f7cd6b6de3ff366d73ea1e7a05ae8f6ac4ba
Nandeesh94 <nandeesh.flipkart@gmail.com>
2025-01-17 20:23:34 +0530

    Update SonarQube.10.sh

--- SonarQube.10.sh
+++ SonarQube.10.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 # Update and install required packages
 
-#sudo yum update -y
-#sudo amazon-linux-extras enable corretto17
-#sudo yum install java-17-amazon-corretto -y
-#sudo yum install wget unzip -y
+sudo yum update -y
+sudo amazon-linux-extras enable corretto17
+sudo yum install java-17-amazon-corretto -y
+sudo yum install wget unzip -y
 
 # Download and install SonarQube
 cd /opt/
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.2.0.77652.zip
 sudo unzip sonarqube-10.2.0.77652.zip
-sudo yum install java-17-amazon-corretto -y
 sudo useradd sonar
 sudo chown sonar:sonar sonarqube-10.2.0.77652 -R
 sudo chmod 777 sonarqube-10.2.0.77652 -R
-su - sonar 
+sudo su - sonar -c
 cd /opt
-cd sonarqube-10.2.0.77652/bin/linux-x86-64/
+cd sonarqube-10.2.0.77652/bin/linux/
 ./sonar.sh start
-./sonar status
+./sonar.sh status
 
 #run this on server manually
 #sh /opt/sonarqube-10.2.0.77652/bin/linux-x86-64/sonar.sh start
